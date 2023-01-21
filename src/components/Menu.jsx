ae8066c61c99d2104ce933b0ec947f2d406a88d9
Natalie Djerf <nadjerf@gmail.com>
2023-01-21 16:39:26 -0500

    Fix menu rerendering

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -10,133 +10,143 @@ import BbqTools from "../images/bbq-tools.inline.svg"
 import Cocktail from "../images/cocktail.inline.svg"
 import Drumstick from "../images/drumstick.inline.svg"
 
-const sections = {
-  box: {
-    display_name: "By the Box",
-    section_description: "Prices are small, medium, and large boxes",
-    additional_text: "",
-    one_size: false,
-    items: [],
-  },
-  broiler: {
-    display_name: "Fresh from The Broiler",
-    section_description: "Served with rice pilaf and a small garden salad",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-  plates: {
-    display_name: "Plates",
-    section_description: "Served with french fries and homemade coleslaw",
-    additional_text: "Sweet potato fries available for additional fee",
-    one_size: true,
-    items: [],
-  },
-  baskets: {
-    display_name: "Baskets",
-    section_description: "Served with french fries",
-    additional_text: "Sweet potato fries available for additional fee",
-    one_size: true,
-    items: [],
-  },
-  sandwiches: {
-    display_name: "Deli Sandwiches",
-    section_description: "Served with chips and a pickle spear",
-    additional_text:
-      "Choice of deli roll, white bread, wheat bread, or Syrian pocket",
-    one_size: true,
-    items: [],
-  },
-  griddle: {
-    display_name: "Off the Griddle",
-    section_description: "Order as a platter to add french fries and coleslaw",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-  wraps: {
-    display_name: "Wraps",
-    section_description: "Served with chips and a pickle spear",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-  subs: {
-    display_name: "Subs",
-    section_description: "Served with chips and a pickle spear",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-  salads: {
-    display_name: "Salads",
-    section_description:
-      "Choice of Italian, oil and vinegar, Greek, ranch, caesar, balsamic vinaigrette, or blue cheese dressing",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-  sides: {
-    display_name: "Sides",
-    section_description: "",
-    additional_text: "",
-    one_size: false,
-    items: [],
-  },
-  kids: {
-    display_name: "Kiddie Meals",
-    section_description: "Served with kids french fries and soda",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-  favorites: {
-    display_name: "Favorites",
-    section_description: "",
-    additional_text: "",
-    one_size: true,
-    items: [],
-  },
-}
-
-const beverage_sections = {
-  cocktails: {
-    display_name: "Cocktails",
-    items: [],
-  },
-  frozen: {
-    display_name: "Frozen",
-    items: [],
-  },
-  beer: {
-    display_name: "Beer, Seltzer, Cider",
-    items: [],
-  },
-  draft: {
-    display_name: "Draft",
-    items: [],
-  },
-  wine: {
-    display_name: "Wine",
-    items: [],
-  },
-  liquor: {
-    display_name: "Liquor",
-    items: [],
-  },
-}
-
 const Menu = ({ data }) => {
-  data.forEach(item => {
-    const menuItem = item?.childMarkdownRemark?.frontmatter
-    const category = menuItem?.category
+  const [config, setConfig] = React.useState({ food: {}, beverage: {} })
 
-    if (sections[category]) {
-      sections[category].items.push(menuItem)
-    } else if (beverage_sections[category]) {
-      beverage_sections[category].items.push(menuItem)
-    }
-  })
+  const sections = {
+    box: {
+      display_name: "By the Box",
+      section_description: "Prices are small, medium, and large boxes",
+      additional_text: "",
+      one_size: false,
+      items: [],
+    },
+    broiler: {
+      display_name: "Fresh from The Broiler",
+      section_description: "Served with rice pilaf and a small garden salad",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+    plates: {
+      display_name: "Plates",
+      section_description: "Served with french fries and homemade coleslaw",
+      additional_text: "Sweet potato fries available for additional fee",
+      one_size: true,
+      items: [],
+    },
+    baskets: {
+      display_name: "Baskets",
+      section_description: "Served with french fries",
+      additional_text: "Sweet potato fries available for additional fee",
+      one_size: true,
+      items: [],
+    },
+    sandwiches: {
+      display_name: "Deli Sandwiches",
+      section_description: "Served with chips and a pickle spear",
+      additional_text:
+        "Choice of deli roll, white bread, wheat bread, or Syrian pocket",
+      one_size: true,
+      items: [],
+    },
+    griddle: {
+      display_name: "Off the Griddle",
+      section_description:
+        "Order as a platter to add french fries and coleslaw",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+    wraps: {
+      display_name: "Wraps",
+      section_description: "Served with chips and a pickle spear",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+    subs: {
+      display_name: "Subs",
+      section_description: "Served with chips and a pickle spear",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+    salads: {
+      display_name: "Salads",
+      section_description:
+        "Choice of Italian, oil and vinegar, Greek, ranch, caesar, balsamic vinaigrette, or blue cheese dressing",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+    sides: {
+      display_name: "Sides",
+      section_description: "",
+      additional_text: "",
+      one_size: false,
+      items: [],
+    },
+    kids: {
+      display_name: "Kiddie Meals",
+      section_description: "Served with kids french fries and soda",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+    favorites: {
+      display_name: "Favorites",
+      section_description: "",
+      additional_text: "",
+      one_size: true,
+      items: [],
+    },
+  }
+
+  const beverage_sections = {
+    cocktails: {
+      display_name: "Cocktails",
+      items: [],
+    },
+    frozen: {
+      display_name: "Frozen",
+      items: [],
+    },
+    beer: {
+      display_name: "Beer, Seltzer, Cider",
+      items: [],
+    },
+    draft: {
+      display_name: "Draft",
+      items: [],
+    },
+    wine: {
+      display_name: "Wine",
+      items: [],
+    },
+    liquor: {
+      display_name: "Liquor",
+      items: [],
+    },
+  }
+
+  React.useEffect(() => {
+    data.forEach(item => {
+      const menuItem = item?.childMarkdownRemark?.frontmatter
+      const category = menuItem?.category
+
+      if (sections[category]) {
+        sections[category].items.push(menuItem)
+      } else if (beverage_sections[category]) {
+        beverage_sections[category].items.push(menuItem)
+      }
+    })
+
+    setConfig({
+      food: sections,
+      beverage: beverage_sections,
+    })
+  }, [])
 
   const buildItem = item => {
     return (
@@ -221,7 +231,7 @@ const Menu = ({ data }) => {
           alt=""
           className="col11 row12 hideMobile"
         />
-        {Object.entries(sections).map(([key, section]) =>
+        {Object.entries(config.food).map(([key, section]) =>
           buildSection(section)
         )}
       </div>
@@ -238,7 +248,7 @@ const Menu = ({ data }) => {
         />
         <Beer role="presentation" alt="" className="col4 row5 hideMobile" />
         <Soda role="presentation" alt="" className="col12 row6  hideMobile" />
-        {Object.entries(beverage_sections).map(([key, section]) =>
+        {Object.entries(config.beverage).map(([key, section]) =>
           buildSection(section, true)
         )}
       </div>
