b29c5648da279048583e75221a075e3a810042fd
Natalie Djerf <nadjerf@gmail.com>
2023-03-19 12:55:56 -0400

    Press page

---
+++ src/components/Press.jsx
@@ -0,0 +1,66 @@
+import React from "react"
+
+const Press = () => {
+  return (
+    <>
+      <h1 className="h1">In the Press</h1>
+      <div className="">
+        <div className="galleryGrid foodGrid gap5 justifyCenter paddingMin">
+          <div>
+            <h4>Phantom Gourmet</h4>
+            <iframe
+              className="pressIframe"
+              src="https://www.youtube.com/embed/GMRguJz_vFs"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div>
+            <h4>Phantom Gourmet - Hidden Jewel</h4>
+            <iframe
+              className="pressIframe"
+              src="https://www.youtube.com/embed/k14rf3Bd1Zs"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div>
+            <h4>Fox News - Taste of the Town</h4>
+            <iframe
+              className="pressIframe"
+              src="https://www.youtube.com/embed/tE6QUCcykuM"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+          <div>
+            <h4>Patriot Ledger - Mystery of the Sea</h4>
+            <iframe
+              className="pressIframe"
+              src="https://www.youtube.com/embed/5_PFOgcGou8"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+        <div>
+          <div>
+            <a href="https://boscp.com/2019/04/19/tonys-clam-shop-quincys-historical-seaside-attraction/">
+              Tony’s Clam Shop Quincy’s Historical Seaside Attraction
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Press
---
+++ src/pages/press.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import Press from "../components/Press"
+
+const PressPage = () => {
+  return (
+    <Layout>
+      <Seo title="Press" />
+      <Press />
+    </Layout>
+  )
+}
+
+export default PressPage
